d7d600db4163a2306b883daebe9736a9e8cd1cb1
Alex Nikiforov <anikiforov@mobileguard.com>
2017-12-20 23:34:26 -0500

    Blind commit

--- src/main/java/WalletController.java
+++ src/main/java/WalletController.java
@@ -1,32 +1,10 @@
-import java.awt.Dimension;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 
-import java.util.EnumMap;
-import java.util.Map;
-
-
-import javax.swing.JFrame;
-
-
-
-import com.github.sarxos.webcam.Webcam;
-import com.github.sarxos.webcam.WebcamPanel;
-import com.github.sarxos.webcam.WebcamResolution;
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.BinaryBitmap;
-import com.google.zxing.DecodeHintType;
-import com.google.zxing.LuminanceSource;
-import com.google.zxing.MultiFormatReader;
-import com.google.zxing.NotFoundException;
-import com.google.zxing.Result;
 import com.google.zxing.Writer;
 import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.QRCodeWriter;
 
 
@@ -57,7 +35,8 @@ public class WalletController {
 
 
 	public String scanCode(){
-		return scanner.scanCode();
+		//return scanner.scanCode();
+		return scanner.scanCodeContiniously();
 	}
 
 
