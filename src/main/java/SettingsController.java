f5523427adcf362a27ab1d9bacea53d34e9153ac
Alex Nikiforov <anikiforov@mobileguard.com>
2017-12-20 23:39:52 -0500

    Another blind commit

--- src/main/java/SettingsController.java
+++ src/main/java/SettingsController.java
@@ -1,47 +1,36 @@
-import com.github.sarxos.webcam.Webcam;
-import com.github.sarxos.webcam.WebcamPanel;
-import com.github.sarxos.webcam.WebcamResolution;
-import com.google.zxing.*;
-import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
-import com.google.zxing.common.HybridBinarizer;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.image.BufferedImage;
-import java.io.*;
-import java.util.EnumMap;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class SettingsController {
     QRScanner scanner = new QRScanner();
     String address;
 
-//This section is for the change address section
-    public SettingsController(){
+    //This section is for the change address section
+    public SettingsController() {
+    		super();
     }
 
-    public boolean addressWrite(String newAddress){
-        try {
-            PrintWriter out = new PrintWriter("ETHaddress");
+    public boolean addressWrite(String newAddress) {
+    		PrintWriter out = null;
+    		try {
+    			out = new PrintWriter("ETHaddress");
             out.println(newAddress);
             System.out.println("file printed");
         } catch (FileNotFoundException e) {
             System.out.println("ETHaddress file not found");
+        } finally {
+        		if (null != out) out.close();
         }
-
         return true;
     }
 
     public String addressRead(){
         try (BufferedReader br = new BufferedReader(new FileReader("ETHaddress"))) {
             address = br.readLine();
-
-    } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
@@ -52,7 +41,8 @@ public class SettingsController {
     }
 
     public String scanCode(){
-        return scanner.scanCode();
+        //return scanner.scanCode();
+        return scanner.scanCodeContiniously();
     }
 
     public String returnMiningAddress(){
